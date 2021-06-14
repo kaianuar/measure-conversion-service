2a1d837c13cc3a0ff5ac36da96e96a463abcbcf0
Khairull Anuar Jamlus <anuar.jamlus@gmail.com>
2021-06-14 12:14:10 +0800

    Update the Measurement abstract class to use the conversion trait

--- app/Classes/MeasurementAbstractClass.php
+++ app/Classes/MeasurementAbstractClass.php
@@ -2,7 +2,11 @@
 
 namespace App\Classes;
 
+use App\Traits\ConversionTrait;
+
 abstract class MeasurementAbstractClass implements MeasurementInterface
 {
+    use ConversionTrait;
+
     abstract public function total(string $outputType, array $measurements) : float;
 }
